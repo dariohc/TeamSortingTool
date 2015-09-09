db5e164f7bf216a2c500008a51e69915935179cd
Dario Hermida <dariohc@gmail.com>
2015-09-09 15:23:59 +0200

    almost there checking the result calendar

--- GeneralTeamsSorting/main.py
+++ GeneralTeamsSorting/main.py
@@ -6,13 +6,13 @@ Created on 8 Sep 2015
 #it works only for 2**k (4,8,16,32, teams due to the sorting algorithm.
 #script to generate matches between teams
 #it is required to have all matches evenly distributed
-
+import random
 
 #variables
-total = 8
+total = 14
 calendar = []
 pairings = []
-games = total / 2
+games = int(total) // 2
 weeks = total - 1
 playing = []
  
@@ -45,13 +45,20 @@ print_list(pairings)
 print (len(pairings))
 
 print_list(calendar)
+random.shuffle(pairings)
+print_list(pairings) 
 #now we have al matches to be played
 #for weeks in range (0,weeks+1):
 counter_games = 0
 counter_week = 0
-
-while len(pairings) >= total/2:
-#while counter_week >= 3 :   
+backup_pairings = pairings
+while len(pairings) >= 1:   
+    if (counter_week % total) == 0:
+        print('restart')
+        pairings = backup_pairings
+        random.shuffle(pairings)
+        playing = []
+        counter_games = 0
     for search in pairings:
         if check_team(search):
             continue
@@ -64,14 +71,35 @@ while len(pairings) >= total/2:
                 playing = []
                 counter_games = 0
                 counter_week += 1
-    print (calendar)  
+    #print (calendar)  
     for element in calendar:
         if element in pairings: pairings.remove(element)
     print (len(pairings)) 
-    print (pairings)
+    #print (pairings)
     counter_week += 1
+    print(counter_week)
+    
 
-#for i in range(0,weeks):
-#    for j in range(0,games):
-#        print (calendar[j+i*games])
-#    print ('>>>>')
+print_list(calendar)
+print(len(calendar))
+
+check_games = []
+print (games, weeks)
+for i in range(0,weeks):
+    print ('>>>>',i+1)
+    for j in range(0,games):
+        print (calendar[j+i*games])
+        check_games.append(calendar[j+i*games][0])
+        check_games.append(calendar[j+i*games][1])
+
+total_loop = 0
+games_counter = 0
+for i in range(1,total):
+        for k in check_games:
+            if check_games[k] == i :
+                games_counter+=1    
+            total_loop +=1    
+            if games_counter > 1 and total_loop < 7: print (i, 'team is NOK')
+            if total_loop == 7 : 
+                total_loop = 0
+                games_counter = 0
