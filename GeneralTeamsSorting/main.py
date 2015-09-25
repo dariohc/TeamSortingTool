ef53d9886c19d70a67a579784e7607b7b0735c71
Dario Hermida <dariohc@gmail.com>
2015-09-25 16:02:24 +0200

    radical change of team sorting

--- GeneralTeamsSorting/main.py
+++ GeneralTeamsSorting/main.py
@@ -16,7 +16,7 @@ pairings = []
 games = int(total) // 2
 weeks = total - 1
 playing = []
- 
+position = 0
 # for k in range(0,weeks+1):
 # calendar.append(k)
 
@@ -30,12 +30,32 @@ def print_list(aux_list):
     print('.....')
 
 
-def check_team(aux_list):
+def insert_team(current_game):
     # returns True if there is a game
-    if (aux_list[0] in playing) or (aux_list[1] in playing):
-        return True
-    else:
-        return False
+    print_list(calendar)
+    print(position)
+    global position
+    attempts = 0
+    print(current_game, 'this is my current game')
+    while attempts < weeks:
+        for ka in range(0, games):
+            if ((current_game[0] == calendar[position][ka][0]) or (current_game[0] == calendar[position][ka][1]) or
+               (current_game[1] == calendar[position][ka][0]) or (current_game[1] == calendar[position][ka][1])):
+                print('there is a match')
+                position += 1
+                position %= 5
+                attempts += 1
+                print(position, 'this is the position')
+                continue
+            else:
+                calendar[position][ka] = search
+                print("added")
+                return
+        print_list(calendar)
+        position += 1
+        position %= 5
+        attempts += 1
+        input('we are on the loop')
 
 
 def add_playing(aux_list):
@@ -47,40 +67,41 @@ def add_playing(aux_list):
 # create all pairings
 
 
+def initialize_calendar():
+
+    global calendar
+    for i in range(0, weeks):
+            filling = [total+1, total+1]
+            calendar.append(filling)
+    for i in range(0, weeks):
+        for j in range(0, games):
+            filling = [total+1, total+1]
+            calendar[i].append(filling)
+    for i in range(0, weeks):
+            calendar[i].remove(total+1)
+            calendar[i].remove(total+1)
+
 for i in range(0, total):
-    for j in range(0, total):
-        if i != j and i < j:
-            pairings.append([i, j])
+        for j in range(0, total):
+            if i != j and i < j:
+                pairings.append([i, j])
+initialize_calendar()
+
 print('this is my pairings list')
 print_list(pairings)     
 print(len(pairings))
+print_list(calendar)
 
 # now we have al matches to be played
 # for weeks in range (0,weeks+1):
 counter_games = 0
 counter_week = 0
 backup_pairings = pairings
+input('please press a button')
 while len(pairings) >= 1:
     for search in pairings:
-        if check_team(search):
-            continue
-        else:
-            calendar.append(search)
-            add_playing(search)
-            print ("added")
-            counter_games += 1
-            if counter_games%(total/2) == 0:
-                playing = []
-                counter_games = 0
-                counter_week += 1
-    # print(calendar)
-    for element in calendar:
-        if element in pairings:
-            pairings.remove(element)
-    print(len(pairings))
-    # print(pairings)
-    counter_week += 1
-    print(counter_week)
+        insert_team(search)  # we iterate over the whole list of pairings
+    input('please press a button')
 
 print_list(calendar)
 print(len(calendar))
